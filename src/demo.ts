374c15a2afb53ffc8e460063025deb6bd15793bd
qudsi513 <ajianhar513@gmail.com>
2025-11-28 23:45:25 +0800

    challenge5

---
+++ src/demo.ts
@@ -0,0 +1,8 @@
+import { addBook, listBooks, searchBook } from "./challenge1";
+
+addBook("The Great Gatsby", "F. Scott Fitzgerald", 1925);
+addBook("1984", "George Orwell", 1949);
+
+listBooks();
+
+searchBook("1984");
